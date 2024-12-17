35d358f54cc17c1e1e3eee05f43ae55a8f3c2efe
201210302 <90252213+201210302@users.noreply.github.com>
2024-12-18 01:13:11 +0900

    Add files via upload

---
+++ utils/data.js
@@ -0,0 +1,291 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "01_arcade",
+            "elements": [
+                
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/01_arcade.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/01_arcade_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/01_arcade_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/01_arcade_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/01_arcade.png"
+                },
+            ]
+        },
+        {
+            "title": "01_dot1",
+            "elements": [
+
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/01_dot1.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/01_dot1_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/01_dot1_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/01_dot1_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/01_dot1.png"
+                },
+            ]
+        },
+        
+        {
+            "title": "12_barrel",
+            "elements": [
+
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/12_barrel.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/12_barrel_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/12_barrel_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/12_barrel_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/12_barrel.png"
+                },
+            ]
+        },
+        {
+            "title": "atlas",
+            "elements": [
+
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/atlas.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/atlas_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/atlas_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/atlas_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/atlas.png"
+                },
+            ]
+        },
+        {
+            "title": "kodim05",
+            "elements": [
+
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/kodim05.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/kodim05_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/kodim05_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/kodim05_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/kodim05.png"
+                },
+            ]
+        },
+
+        {
+            "title": "sponza_fabric_green_diff",
+            "elements": [
+
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/sponza_fabric_green_diff.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/sponza_fabric_green_diff_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/sponza_fabric_green_diff_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/sponza_fabric_green_diff_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/sponza_fabric_green_diff.png"
+                },
+            ]
+        },
+        {
+            "title": "vokselia_spawn",
+            "elements": [
+                {
+                    "title": "GT",
+                    "version": "-",
+                    "image": "GT/vokselia_spawn.png"
+                },
+                {
+                    "title": "RealESRGAN (x2)",
+                    "version": "-",
+                    "image": "RealESRGAN/vokselia_spawn_out.png"
+                },
+                {
+                    "title": "Modified parameter",
+                    "version": "-",
+                    "image": "Modified parameter/vokselia_spawn_out.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/vokselia_spawn_PC_NEAREST.png"
+                },
+                {
+                    "title": "Input image (1/2 size)",
+                    "version": "-",
+                    "image": "input/vokselia_spawn.png"
+                },
+            ]
+        }
+    ],
+    "stats_short": [
+        {
+            "title": "short-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "128",
+                        "128",
+                        "256",
+                        "256",
+                        "256"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.086589",
+                        "0.057743",
+                        "0.032066",
+                        "0.027440",
+                        "0.009671"
+                    ]
+                }
+            ]
+        }
+    ],
+    "stats_long": [
+        {
+            "title": "long-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "512",
+                        "512",
+                        "1024",
+                        "1024",
+                        "1024"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.022007",
+                        "0.016828",
+                        "0.009719",
+                        "0.007926",
+                        "0.003718"
+                    ]
+                }
+            ]
+        }
+    ]
+}
---
+++ utils/data2.js
@@ -0,0 +1,322 @@
+const data =
+{
+    "imageBoxes": [
+        
+        {
+            "title": "ILSVRC2012_val_00002322",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00002322.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00002322_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00002322.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00002322_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00003328",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00003328.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00003328_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00003328.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00003328_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00006865",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00006865.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00006865_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00006865.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00006865_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00008023",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00008023.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00008023_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00008023.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00008023_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00014709",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00014709.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00014709_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00014709.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00014709_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00028648",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00028648.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00028648_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00028648.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00028648_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00039791",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00039791.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00039791_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00039791.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00039791_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00022655",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00022655.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00022655_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00022655.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00022655_PC_out.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00002287",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00002287.png"
+                },
+                {
+                    "title": "RealESRGAN (x4)",
+                    "version": "-",
+                    "image": "RealESRGAN/ILSVRC2012_val_00002287_out.png"
+                },
+                {
+                    "title": "DeSRA (x4)",
+                    "version": "-",
+                    "image": "DeSRA/ILSVRC2012_val_00002287.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00002287_PC_out.png"
+                },
+
+            ]
+        },
+        
+       
+       
+        
+     
+    ],
+    "stats_short": [
+        {
+            "title": "short-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "128",
+                        "128",
+                        "256",
+                        "256",
+                        "256"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.086589",
+                        "0.057743",
+                        "0.032066",
+                        "0.027440",
+                        "0.009671"
+                    ]
+                }
+            ]
+        }
+    ],
+    "stats_long": [
+        {
+            "title": "long-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "512",
+                        "512",
+                        "1024",
+                        "1024",
+                        "1024"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.022007",
+                        "0.016828",
+                        "0.009719",
+                        "0.007926",
+                        "0.003718"
+                    ]
+                }
+            ]
+        }
+    ]
+}
