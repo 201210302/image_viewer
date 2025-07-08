a7dba3f29a88d2854e155d77c12863d27ae2c149
HyejuKim <90252213+201210302@users.noreply.github.com>
2025-07-08 21:26:22 +0900

    Add files via upload

---
+++ utils/data3.js
@@ -0,0 +1,270 @@
+const data =
+{
+    "imageBoxes": [
+        
+        {
+            "title": "ILSVRC2012_val_00023491",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00023491.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00023491_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00023491_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00023491_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00002349",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00002349.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00002349_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00002349_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00002349_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00022655",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00022655.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00022655_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00022655_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00022655_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00030793",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00030793.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00030793_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00030793_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00030793_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00032728",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00032728.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00032728_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00032728_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00032728_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00019003",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00019003.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00019003_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00019003_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00019003_realsr_PC.png"
+                },
+
+            ]
+        },
+        {
+            "title": "ILSVRC2012_val_00019534",
+            "elements": [
+
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "input/ILSVRC2012_val_00019534.png"
+                },
+                {
+                    "title": "Classical_SR (x2)",
+                    "version": "-",
+                    "image": "Classical_SR/ILSVRC2012_val_00019534_SwinIR.png"
+                },
+                {
+                    "title": "Real_SR (x2)",
+                    "version": "-",
+                    "image": "Real_SR/ILSVRC2012_val_00019534_SwinIR.png"
+                },
+                {
+                    "title": "TexSR(Ours)",
+                    "version": "-",
+                    "image": "TexSR/ILSVRC2012_val_00019534_realsr_PC.png"
+                },
+
+            ]
+        },
+        
+        
+        
+       
+       
+        
+     
+    ],
+    "stats_short": [
+        {
+            "title": "short-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "128",
+                        "128",
+                        "256",
+                        "256",
+                        "256"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.086589",
+                        "0.057743",
+                        "0.032066",
+                        "0.027440",
+                        "0.009671"
+                    ]
+                }
+            ]
+        }
+    ],
+    "stats_long": [
+        {
+            "title": "long-term rendering",
+            "labels": [
+                "Input image (PT)",
+                "Input image (CRN)",
+                "Uni-kernel",
+                "Cross-weighting",
+                "Ours"
+            ],
+            "series": [
+                {
+                    "label": "spp",
+                    "data": [
+                        "512",
+                        "512",
+                        "1024",
+                        "1024",
+                        "1024"
+                    ]
+                },
+                {
+                    "label": "relative L2",
+                    "data": [
+                        "0.022007",
+                        "0.016828",
+                        "0.009719",
+                        "0.007926",
+                        "0.003718"
+                    ]
+                }
+            ]
+        }
+    ]
+}
